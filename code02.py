4bb5eda3c69ee5ef8f39b7d3b10061272755b11b
unknown <97228571@qq.com>
2017-08-14 22:21:27 +0800

    修改add

--- code02.py
+++ code02.py
@@ -1,26 +1,36 @@
 # encoding=utf-8
 
 
-user = []
-user_list = []
+users = []
 
 while True:
     str = input("请输入要操作的功能名称(find/list/add/delete/update/exit): ")
 
     if str == "add":
         print("请完善下述信息项")
-        str_name = input("请输入用户名：")
-        str_age = input("请输入年龄：")
-        str_mobile = input("请输入联系方式：")
+        name = input("请输入用户名：")
+        age = input("请输入年龄：")
+        mobile = input("请输入联系方式：")
 
-        user_tuple = (str_name,str_age,str_mobile)
-        user.extend(user_tuple)
-        print(user)
+        user_tuple = (name,age,mobile)
 
-    if str == "delete":
+        is_exists = False
+
+        for user in users:
+            if user[0] == name:
+                print("添加用户失败，失败原因：用户名已存在")
+                is_exists = True
+                break
+            
+        if not is_exists:
+            users.append(user_tuple)
+            print("添加用户成功")
+
+    elif str == "delete":
         str_name1 = input("请输入要删除的用户名：")
         if str_name1 in list_str:
             pass
 
-    if str == "exit":
-    	break
+    elif str == "exit":
+        print("程序将退出，感谢使用")
+        break
