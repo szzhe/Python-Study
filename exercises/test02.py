86291f19212b1ac9aa3b9ec30eab142ca2556a4b
songzhenzhe <97228571@qq.com>
2017-09-27 17:56:16 +0800

    x

--- exercises/test02.py
+++ exercises/test02.py
@@ -1,56 +1,4 @@
 
-# 11.3.3 前向引用
-
-# def foo():
-#   print('in foo()')
-#   bar()
-
-# def bar():
-#   print("in bar()")
-
-# foo()
-
-'''
-这段代码是正确的因为即使(在foo()中)对bar()进行的调用出现在bar()的定义之前，但foo()本身不是在bar()声明之前被调用的。 换句话说，我们声明foo()，然后再声明bar()，接着调用foo()，
-但是到那时，bar()已经存在了，所以调用成功。 
-'''
-# 11.3.4 函数属性
-
-# def foo():
-#     '''foo() -- properly created doc string:szzhe'''
-
-# def bar():
-#     pass
-
-# # 在函数声明外定义一个文档字串
-# bar.__doc__ = 'Oops,forgot the doc above'
-# bar.version = 0.1
-
-# help(foo)
-# print(bar.version)
-# print("bar.__doc__:", bar.__doc__)
-# print("foo.__doc__:", foo.__doc__)
-
-
-# 11.3.5 内部/内嵌函数
-
-'''方式一：最明显的创造内部函数的方法是在外部函数的定义体内定义函数(用def关键字)'''
-
-# def foo():
-#     def bar():
-#         print('bar() called')
-#     print('foo() called')
-#     bar()
-# # foo()
-
-# bar() # NameError: name 'bar' is not defined
-
-'''内部函数一个有趣的地方在于整个函数体都在外部函数的作用域之内。如果没有任何对bar()的外部引用，那么除了在函数体内，任何地方都不能对其进行调用'''
-
-'''方式二：函数体内创建函数对象的方式是使用lambda语句。
-   如果内部函数的定义包含了在外部函数里定义的对象的引用(这个对象甚至可以是在外部函数之外)，内不函数会被称为闭包(closure)的特别之物
-'''
-
 # 11.3.6 *函数(与方法)装饰器
 
 '''装饰器是在函数调用之上的装饰。这些装饰仅是当声明一个函数或者方法的时候，才会应用的额外调用
@@ -76,104 +24,45 @@
 # deco(myfunc)
 # myfunc()
 
-# 作用域
-# a_string = "This is a global variable"
-# def test(arg):
-#     z = 1
-#     a_string = "test"
-#     print(locals()) # locals() 基于字典的访问局部，字典的键就是变量名，字典的值就是那些变量的值
-# test(4) # {'a_string': 'test', 'z': 1, 'arg': 4}
-# test('doulaixuexi') # {'a_string': 'test', 'z': 1, 'arg': 'doulaixuexi'}
-# print(globals())
-
-'''globals 函数返回一个全局变量的字典，包括所有导入的变量。 {...,'a_string': 'This is a global variable'，...}'''
-
-# 变量生存周期
-
-'''函数foo的命名空间随着函数调用开始而开始，结束而销毁'''
+# import logging
 
 # def foo():
-#      x = 1
+#     print('i am foo')
+#     logging.info("foo is running")
 # foo()
-# print x # SyntaxError: Missing parentheses in call to 'print'
-
-# 闭包(closure):是一种引用了外部变量的函数对象，无论该变量所处的作用域是否还存在于内存中。
 
 
-def generate_power_func(n):
-    print("id(n): %X" % (id(n)))  # %X 无符号十六进制
-
-    def nth_power(x):
-        return x**n
-    print("id(nth_power): %X" % id(nth_power))
-    return nth_power
+def Before(request, kargs):
+    print('before')
 
 
-kk = generate_power_func(4)  # 调用函数，会生成一个nth_power函数对象；且将结果返回给一个变量kk
-# print(repr(kk))  # repr() 函数将对象转化为供解释器读取的形式，返回一个对象的 string 格式
-'''
-id(n): 7287c710
-id(nth_power): 1a4715398c8
-'''
-del generate_power_func  # 将函数 generate_power_func 函数从全局命名空间删除
-print(kk(2))  # 调用闭包函数 kk = 16
-print(kk.__closure__)
-print(kk.__closure__[0].cell_contents)
-'''结论：函数对象nth_power是由generate_power_func产生的一个闭包，闭包会保留来自外围作用域变量的信息。'''
-
-'''************************************************************'''
-# def outer():
-#     x = 1
-#     def inner():
-#         print(x)
-#     return inner
-# foo = outer()
-# print(foo.__closure__)
-# # __closure__ 对象返回一个由 cell 对象组成的元组，cell 对象记录了定义在外围作用域的变量信息。
-# # (<cell at 0x000001F8BA898498: int object at 0x000000007287C6B0>,)
-# # 对于那些不是闭包的函数对象来说，__closure__ 属性值为 None
-# print(type(foo.__closure__[0]))  # <class 'cell'>
-# print(foo.__closure__[0].cell_contents)  # 1
-
-'''************************************************************'''
-
-# def line_conf():
-#     def line(x):
-#         return 2 * x + 1
-#     print(line(5))
-
-# line_conf()
-
-'''************************************************************'''
-# def line_conf():
-#     b = 15
-#     def line(x):
-#         return 2*x+b
-#     return line
-
-# b = 5
-# my_line = line_conf()
-# print(my_line.__closure__)
-# print(my_line.__closure__[0].cell_contents)
-
-'''************************************************************'''
-# def line_conf(a, b):
-#     i = a * b # 20
-#     def line(x):
-#     	nonlocal i
-#     	i = i + x # 25
-#     	return i * x + b
-#     return line
-
-# line1 = line_conf(4, 5)
-# print(line1(5))
+def After(request, kargs):
+    print('after')
 
 
-def print_msg(msg):
-    '''This is the outer enclosing function'''
-    def printer():
-    '''This is the nested function'''
-        print(msg)
+def Filter(before_func, after_func):
+    def outer(main_func):
+        def wrapper(request, kargs):
 
-# printer()
-print_msg("Hello"))
+            before_result = before_func(request, kargs)
+            if(before_result != None):
+                return before_result
+
+            main_result = main_func(request, kargs)
+            if(main_result != None):
+                return main_result
+
+            after_result = after_func(request, kargs)
+            if(after_result != None):
+                return after_result
+
+        return wrapper
+    return outer
+
+
+@Filter(Before, After)
+def Index(request, kargs):
+    print('index')
+
+
+Index('Hello', 'World')
--- exercises/test03.py
+++ exercises/test03.py
@@ -1,9 +1,82 @@
 
-### 闭包函数
+# 前向引用
+
+# def foo():
+#   print('in foo()')
+#   bar()
+
+# def bar():
+#   print("in bar()")
+
+# foo()
+
+'''
+这段代码是正确的因为即使(在foo()中)对bar()进行的调用出现在bar()的定义之前，但foo()本身不是在bar()声明之前被调用的。 换句话说，我们声明foo()，然后再声明bar()，接着调用foo()，
+但是到那时，bar()已经存在了，所以调用成功。
+'''
+
+# 函数属性
+
+# def foo():
+#     '''foo() -- properly created doc string:szzhe'''
+
+# def bar():
+#     pass
+
+# # 在函数声明外定义一个文档字串
+# bar.__doc__ = 'Oops,forgot the doc above'
+# bar.version = 0.1
+
+# help(foo)
+# print(bar.version)
+# print("bar.__doc__:", bar.__doc__)
+# print("foo.__doc__:", foo.__doc__)
+
+# 内部/内嵌函数
+
+'''方式一：最明显的创造内部函数的方法是在外部函数的定义体内定义函数(用def关键字)'''
+
+# def foo():
+#     def bar():
+#         print('bar() called')
+#     print('foo() called')
+#     bar()
+# # foo()
+
+# bar() # NameError: name 'bar' is not defined
+
+'''内部函数一个有趣的地方在于整个函数体都在外部函数的作用域之内。如果没有任何对bar()的外部引用，那么除了在函数体内，任何地方都不能对其进行调用'''
+
+'''方式二：函数体内创建函数对象的方式是使用lambda语句。
+   如果内部函数的定义包含了在外部函数里定义的对象的引用(这个对象甚至可以是在外部函数之外)，内不函数会被称为闭包(closure)的特别之物
+'''
+
+# 变量生存周期
+
+'''函数foo的命名空间随着函数调用开始而开始，结束而销毁'''
+
+# def foo():
+#      x = 1
+# foo()
+# print x # SyntaxError: Missing parentheses in call to 'print'
+
+# 作用域
+# a_string = "This is a global variable"
+# def test(arg):
+#     z = 1
+#     a_string = "test"
+#     print(locals()) # locals() 基于字典的访问局部，字典的键就是变量名，字典的值就是那些变量的值
+# test(4) # {'a_string': 'test', 'z': 1, 'arg': 4}
+# test('doulaixuexi') # {'a_string': 'test', 'z': 1, 'arg': 'doulaixuexi'}
+# print(globals())
+
+'''globals 函数返回一个全局变量的字典，包括所有导入的变量。 {...,'a_string': 'This is a global variable'，...}'''
+
+# 闭包函数
 
 '''
 闭包存在有什么意义呢？为什么需要闭包？
-个人认为，闭包存在的意义就是它夹带了外部变量（私货），如果它不夹带私货，它和普通的函数就没有任何区别。同一个的函数夹带了不同的私货，就实现了不同的功能。
+个人认为，闭包存在的意义就是它夹带了外部变量(私货)，如果它不夹带私货，它和普通的函数就没有任何区别。同一个的函数夹带了不同的私货，就实现了不同的功能。
 其实你也可以这么理解，闭包和面向接口编程的概念很像，可以把闭包理解成轻量级的接口封装。
 
 Nonlocal variable是相对于某个函数来说的，指的是这个函数所调用的在本函数作用域之外的变量。
@@ -21,16 +94,22 @@ Nested function指的被定义在一个函数(outer enclosing function)中的函
 # 在这个例子中函数printer就是一个nested function，而变量msg就是一个nonlocal variable。这里需要注意的是，printer虽然可以访问msg，但是不可以改变它。
 # 如果必须要更改这个变量的值，，在Python3中新引入的nonlocal语句可以解决。
 # Nonlocal 与 global 的区别在于 nonlocal 语句会去搜寻本地变量与全局变量之间的变量，其会优先寻找层级关系与闭包作用域最近的外部变量。
+
 # 将print_msg("Hello")返回的函数赋值给another，再调用another函数时，发现已经离开了print_msg函数的作用域，但是"Hello"已经被绑定给another,所以仍然能够正常调用。
 # 删除print_msg之后，another仍然能够正常调用。
-
 # del print_msg
 # # print_msg("Hello") # NameError: name 'print_msg' is not defined
 # another() # Hello
 
-# 支持将函数当成对象使用的编程语言，一般都支持闭包。比如Python, JavaScript
+# print(another.__closure__)
+# (<cell at 0x0000025F94C75708: str object at 0x0000025F94D09298>,)
 
-### 闭包的应用
+# # __closure__ 对象返回一个由 cell 对象组成的元组，cell 对象记录了定义在外围作用域的变量信息。
+# 对于那些不是闭包的函数对象来说，__closure__ 属性值为 None
+
+# print(another.__closure__[0].cell_contents) # Hello
+
+# 闭包的应用
 
 '''当符合下面几个条件时就形成了闭包：
 1.有一个Nested function
@@ -47,13 +126,15 @@ Nested function指的被定义在一个函数(outer enclosing function)中的函
 #     def make_voice(voice):
 #         return "{} sings {}".format(animal,voice)
 #     return make_voice
-# dog = make_sing("dog") # dog = make_voice()；animal = "dog"
-# print(dog("wong")) # make_voice("wong") -> animal = "dog"；voice = "wong"
+# dog = make_sing("dog")
+# print(dog("wong"))
 
-### 什么是装饰器？
+# 什么是装饰器？
 
 ''' 
-装饰器的功能是将被装饰的函数a()当作参数传递给与装饰器@a对应的函数b()(名称相同的函数)，并返回包装后的被装饰的函数。
+装饰器本质上是一个Python函数，它可以让其他函数在不需要做任何代码变动的前提下增加额外功能，装饰器的返回值也是一个函数对象。
+
+装饰器的功能是将被装饰的函数a()当作参数传递给与装饰器@a对应的函数b()(与@a名称相同的函数)，并返回包装后的被装饰的函数。
 简而言之：@a 就是将 b 传递给 a()，并返回新的 b = a(b)
 '''
 
@@ -70,37 +151,119 @@ Nested function指的被定义在一个函数(outer enclosing function)中的函
 3.dobi()函数执行，调用qinfeng()，qinfeng()打印“dobi”
 '''
 
-### 闭包与装饰器
+# 习题：装饰器有一个参数
 
-# 闭包通常用来实现一个通用的功能，Python中的装饰器就是对闭包的一种应用，只不过装饰器中父函数的参数是一个函数。
+# def w1(func):
+#     def inner(arg):
+#         # 验证1
+#         # 验证2
+#         # 验证3
+#         return func(arg)
+#     return inner
 
-def make_wrap(func):
-    def wrapper(*args):
-        print("before function")
-        func(*args)
-        print("after function")
-    return wrapper
+# @w1
+# def f1(arg):
+#     print('f1')
 
-def make_another(func):
-    def wrapper(*args):
-        print("another begin")
-        func(*args)
-        print("another end")
-    return wrapper
+# f1(3)
 
-@make_another     # 等同于 print_msg = make_another(make_wrap(print_msg))
-@make_wrap        # 等同于 print_msg = make_wrap(print_msg)
-def print_msg(msg): 
-    print(msg) # msg = "Hello"
+# 习题：N个参数
+# def w1(func):
+#     def inner(*args, **kwargs):
+#         # 验证1
+#         # 验证2
+#         # 验证3
+#         return func(*args, **kwargs)
+#     return inner
 
-print_msg("Hello")
+# @w1
+# def f1(arg1, arg2, arg3):
+#     print('f1')
 
-'''
-转换成闭包的形式：
-print_msg = make_wrap(print_msg)
-print_msg = make_another(print_msg)
+# f1(3, 4, 5)
 
-'''
+# 习题：被多个装饰器装饰
 
-''' https://segmentfault.com/a/1190000008955952  '''
+# def make_wrap(func):
+#     def wrapper(*args):
+#         print("before function")
+#         func(*args)
+#         print("after function")
+#     return wrapper
 
+# def make_another(func):
+#     def wrapper(*args):
+#         print("another begin")
+#         func(*args)
+#         print("another end")
+#     return wrapper
+
+# @make_another     # 等同于 print_msg = make_another(make_wrap(print_msg))
+# @make_wrap        # 等同于 print_msg = make_wrap(print_msg)
+# def print_msg(msg):
+#     print(msg) # msg = "Hello"
+
+# print_msg("Hello")
+
+# 习题：
+
+# def logging(level):
+#     def wrapper(func):
+#         def inner_wrapper(*args, **kwargs):
+#             print(
+#                 "[{level}]: enter function {func}()".format(
+#                 level=level,
+#                 func=func.__name__)
+#                 )
+#             return func(*args, **kwargs)
+#         return inner_wrapper
+#     return wrapper
+
+# @logging(level='INFO')
+# def say(something):
+#     print("say {}!".format(something))
+
+# # 如果没有使用@语法，等同于
+# # say = logging(level='INFO')(say)
+
+# @logging(level='DEBUG')
+# def do(something):
+#     print("do {}...".format(something))
+
+# if __name__ == '__main__':
+#     say('hello')
+#     do("my work")
+
+''' 当带参数的装饰器被打在某个函数上时，比如@logging(level='DEBUG')，它其实是一个函数，会马上被执行，只要这个它返回的结果是一个装饰器时，那就没问题 '''
+
+# 习题：很屌的装饰器
+
+# def Before(request, kargs):
+#     print('before')
+
+# def After(request, kargs):
+#     print('after')
+
+# def Filter(before_func, after_func):
+#     def outer(main_func):
+#         def wrapper(request, kargs):
+
+#             before_result = before_func(request, kargs)
+#             if(before_result != None):
+#                 return before_result
+
+#             main_result = main_func(request, kargs)
+#             if(main_result != None):
+#                 return main_result
+
+#             after_result = after_func(request, kargs)
+#             if(after_result != None):
+#                 return after_result
+#         return wrapper
+#     return outer
+
+# @Filter(Before, After)
+# def Index(request, kargs):
+#     print('index')
+
+# Index('Hello', 'World')
